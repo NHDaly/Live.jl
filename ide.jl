ba11d12d5e695e205d58a6540ef22b01594fd364
Nathan Daly <nhdaly@gmail.com>
2018-09-13 00:15:26 -0400

    A working-ish app that shows julia errors as you type!

---
+++ ide.jl
@@ -0,0 +1,119 @@
+using Blink
+
+include("parsefile.jl")
+
+cd(@__DIR__)
+w = Window(Blink.@d(:async=>false))
+#tools(w)
+
+# Set up to allow loading modules
+js(w, Blink.JSString("""
+    window.nodeRequire = require;
+    delete window.require;
+    delete window.exports;
+    delete window.module;
+ """))
+
+load!(w, "frameworks/jquery-3.3.1.js")
+
+# Load codemirror
+load!(w, "frameworks/codemirror-5.40.0/lib/codemirror.js")
+load!(w, "frameworks/codemirror-5.40.0/lib/codemirror.css")
+# Set up for julia:
+load!(w, "frameworks/codemirror-5.40.0/mode/julia/julia.js")
+
+
+html() = """
+  <table width="100%">
+   <tbody>
+    <tr>
+     <td>
+       <textarea id="text-editor">function myScript()\n   return 100\nend\n</textarea>
+     </td>
+     <td>
+       <textarea id="output-area">output</textarea>
+     </td>
+    </tr>
+   </tbody>
+  </table>
+
+  <style>
+    table {
+        border-collapse: collapse;
+    }
+    td, th {
+        border: 1px solid gray;
+        padding: 3px;
+    }
+  </style>
+ """
+
+# Start the editor:
+body!(w, html(), async=false)
+js(w, Blink.JSString(raw"""
+    texteditor = CodeMirror.fromTextArea($("#text-editor")[0], {
+        lineNumbers: true,
+        mode:  "julia"
+    });
+    undefined;  // Don't return the CodeMirror object
+ """))
+js(w, Blink.JSString(raw"""
+    outputarea = CodeMirror.fromTextArea($("#output-area")[0], {
+        readOnly: true,
+        mode:  "julia"
+    });
+    undefined;  // Don't return the CodeMirror object
+ """))
+
+global outputText = @js w outputarea.getValue()
+
+@js w texteditor.on("update",
+    (cm) -> (Blink.msg("editorchange", cm.getValue());
+             console.log("sent msg to julia!");))
+
+# User code parsing + eval
+module UserCode end
+
+numlines(str) = 1+count(c->c=='\n', str)
+
+text = ""
+function editorchange(editortext)
+    try  # So errors don't crash my Blink window...
+        global text, parsed
+        text = editortext
+        parsed = parseall(editortext)
+        lastline = 1
+        setOutputText(1, '\n'^numlines(text))
+        for node in parsed.args
+            isa(node, LineNumberNode) && (lastline = node.line)
+            try
+                Core.eval(UserCode, node)
+            catch e
+                setOutputText(lastline, string(e))
+            end
+        end
+    catch err
+        println("ERROR: $err")
+    end
+    nothing
+end
+
+function setOutputText(line, text)
+    global outputText
+    outputLines = split(outputText, '\n')
+    println("before: $outputLines")
+    textLines = split(text, '\n')
+    while length(outputLines) <= line + length(textLines)
+        push!(outputLines, "")
+    end
+    start, finish = line, line+numlines(text)-1
+    println("$start, $finish")
+    outputLines[start:finish] = textLines
+    # Must use @js_ since no return value required. (@js hangs)
+    #@js_ w outputarea.replaceRange($text, CodeMirror.Pos($start, 0), CodeMirror.Pos($finish, 0))
+    println("after: $outputLines")
+    outputText = join(outputLines, "\n")
+    @js_ w outputarea.setValue($outputText)
+end
+
+Blink.handlers(w)["editorchange"] = editorchange
---
+++ parsefile.jl
@@ -0,0 +1,39 @@
+# https://stackoverflow.com/a/46366560/751061
+# modified from the julia source ./test/parse.jl
+function parseall(str)
+    pos = start(str)
+    exprs = []
+    parsed = nothing
+    while !done(str, pos)
+            linenum = length(str)
+        if pos < length(str)
+            firstnonspace = match(r"\S", str[pos:end])
+            if firstnonspace != nothing
+                index = pos + firstnonspace.offset - 1
+                linenum = 1+count(c->c=='\n', str[1:index])
+            end
+        end
+        push!(exprs, LineNumberNode(linenum, :none))
+        ex, pos = parse(str, pos) # returns next starting point as well as expr
+        if ex isa Symbol
+            push!(exprs, ex)
+        elseif ex isa Expr
+            ex.head == :toplevel ? exs = ex.args : exs = [ex] #see StackOverflow comments for info
+            for ex in exs
+                #if ex.head == :using
+                #    eval(ex)  # eval :using Expressions globally rather than inserting.
+                #else
+                push!(exprs, ex)
+                #end
+            end
+        end
+    end
+    if length(exprs) == 0
+        return nothing
+    #elseif length(exprs) == 1
+    #    return exprs[1]
+    else
+        return Expr(:block, exprs...) # convert the array of expressions
+                                      # back to a single expression
+    end
+end
