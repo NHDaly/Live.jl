c87570be578932c4e0b70f815c96fec960935e0a
Nathan Daly <nhdaly@gmail.com>
2018-10-18 16:00:46 -0400

    Docstring on Live.@test

--- src/Live.jl
+++ src/Live.jl
@@ -2,12 +2,31 @@
 # with the LiveIDE editor.
 # These macros will have no effect if executed outside the LiveIDE editor.
 module Live
-  struct TestLine
-      args
-      lineNode::LineNumberNode
-  end
 
-  macro test(args...)
-      TestLine(args, __source__)
-  end
+"""
+    Live.@test
+
+This is the main interface for Live: Live.@test
+Use it to test a function in the LiveIDE by annotating the function with this
+macro on its own line:
+
+```julia
+using Live
+
+Live.@test a=5 b=zeros(3)
+function foo(a,b)
+    # ...
+end
+```
+"""
+macro test(args...)
+    TestLine(args, __source__)
+end
+
+# This struct is used by the IDE to pass a context (args) into a function when
+# executing it via Live.@test
+struct TestLine
+    args
+    lineNode::LineNumberNode
+end
 end
