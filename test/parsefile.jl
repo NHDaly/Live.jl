6216b7a166c1bcabf1b579cad12cd562110f5cbc
Nathan Daly <nhdaly@gmail.com>
2018-10-21 19:46:57 -0400

    Un-@script test/parsefile.jl to clarify what it's doing.

--- test/parsefile.jl
+++ test/parsefile.jl
@@ -1,12 +1,11 @@
 using Live
-Live.@script()
 
 using Test
 
 include("../ide/parsefile.jl")
 
 Live.@tested(parseall)
-  Test.@testset "..." begin
+Test.@testset "..." begin
   expr1 = "x + 1"
   expr2 = "function foo() return 3 end"
   Test.@test :(x+1) in parseall(expr1*"\n"*expr2).args
@@ -16,4 +15,3 @@ Test.@test length(parseall("a").args) == 2
 
 Test.@test :x in parseall("x").args
 Test.@test :y in parseall("x; y").args
-
