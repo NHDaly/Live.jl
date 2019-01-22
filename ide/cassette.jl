c29dd1d8329550007d0ce2ad926635caf250f3eb
Nathan Daly <nhdaly@gmail.com>
2019-01-22 17:30:48 -0500

    Clean up cassette.jl: comment out tests

--- ide/cassette.jl
+++ ide/cassette.jl
@@ -47,19 +47,31 @@ thunkwrap(linenode::LineNumberNode) =
                 quote LineNodeThunk(LineNumberNode($(linenode.line), $(string(linenode.file))))() end
 thunkwrap(literal) = quote Thunk(()->$literal)() end
 
-@time e = thunkwrap(quote
-    function foo(x)
-        if (x > 0)
-            100
-        elseif (x == 0)
-            x+5;
-        elseif (x < 10)
-            -x;
-        else 0 end
-     end
-     foo(3)
-end)
-@time eval(e)
+#@time e = thunkwrap(quote
+#    function foo(x)
+#        if (x > 0)
+#            100
+#        elseif (x == 0)
+#            x+5;
+#        elseif (x < 10)
+#            -x;
+#        else 0 end
+#     end
+#     foo(3)
+#end)
+#@time eval(e)
+#@time @eval begin
+#    function foo(x)
+#        if (x > 0)
+#            100
+#        elseif (x == 0)
+#            x+5;
+#        elseif (x < 10)
+#            -x;
+#        else 0 end
+#     end
+#     foo(3)
+# end
 
 
 # Now that that's all set up, we can use cassette to capture the values from those thunks!
@@ -78,20 +90,20 @@ function Cassette.execute(ctx::LiveCtx, line::LineNodeThunk)
     nothing
 end
 
-_ctx = LiveCtx(metadata = CollectedOutputs([], 0))
-@time @eval Cassette.overdub($_ctx, ()->$(thunkwrap(quote
-    function foo(x)
-        if (x > 0)
-            100
-        elseif (x == 0)
-            x+5;
-        elseif (x < 10)
-            -x;
-        else 0 end
-    end
-    foo(3)
-end)))
-_ctx.metadata.outputs
+#_ctx = LiveCtx(metadata = CollectedOutputs([], 0))
+#@time @eval Cassette.overdub($_ctx, ()->$(thunkwrap(quote
+#    function foo(x)
+#        if (x > 0)
+#            100
+#        elseif (x == 0)
+#            x+5;
+#        elseif (x < 10)
+#            -x;
+#        else 0 end
+#    end
+#    foo(3)
+#end)))
+#_ctx.metadata.outputs
 
 # ------- top level statements
 
@@ -145,47 +157,47 @@ function Cassette.execute(ctx::LiveCtx, f::ModuleThunk)::Module
     Core.eval(f.parent, f.expr)::Module
 end
 
-_ctx = LiveCtx(metadata = CollectedOutputs([], 0))
-@time @eval Cassette.overdub($_ctx, ()->$(thunkwrap_toplevel(quote
-    module Test
-    module M
-        struct X end
-        G = 20
-        G + 5
-        module Inner
-            println("HI")
-            x = 10
-        end
-        function f(x) x end
-        f(2)
-        println(f)
-        println(typeof(f).name.module)
-    end
-    M.G + 100
-    M.Inner.x
-    function foo(x)
-        y = x
-        if (y > 0)
-            function helper(y)
-              y+100
-            end
-            helper(10)
-        elseif (x == 0)
-            x+5;
-        elseif (x < 10)
-            -x;
-        else 0 end
-    end
-    foo(3)
-    M.f("hey")
-end
-end)))
-@show _ctx.metadata.outputs
-foo
-
-dump(quote f() = 5 end)
-
-Core.eval(Main, quote Core.eval(Main, (()->quote f2(x) = x+1 end)()) end)
+#_ctx = LiveCtx(metadata = CollectedOutputs([], 0))
+#@time @eval Cassette.overdub($_ctx, ()->$(thunkwrap_toplevel(quote
+#    module Test
+#    module M
+#        struct X end
+#        G = 20
+#        G + 5
+#        module Inner
+#            println("HI")
+#            x = 10
+#        end
+#        function f(x) x end
+#        f(2)
+#        println(f)
+#        println(typeof(f).name.module)
+#    end
+#    M.G + 100
+#    M.Inner.x
+#    function foo(x)
+#        y = x
+#        if (y > 0)
+#            function helper(y)
+#              y+100
+#            end
+#            helper(10)
+#        elseif (x == 0)
+#            x+5;
+#        elseif (x < 10)
+#            -x;
+#        else 0 end
+#    end
+#    foo(3)
+#    M.f("hey")
+#end
+#end)))
+#@show _ctx.metadata.outputs
+#foo
+#
+#dump(quote f() = 5 end)
+#
+#Core.eval(Main, quote Core.eval(Main, (()->quote f2(x) = x+1 end)()) end)
 
 # -------- whole file
 
@@ -201,7 +213,7 @@ function liveEvalCassette(expr)
     return ctx.metadata.outputs
 end
 
-@show liveEvalCassette(parseall(read("$(@__DIR__)/../examples/example2.jl", String)))
+#@show liveEvalCassette(parseall(read("$(@__DIR__)/../examples/example2.jl", String)))
 
 
 # -----------
