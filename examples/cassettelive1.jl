26dde75b38e1071bf7a2ed2e548a9b9bd312ab4d
Nathan Daly <nhdaly@gmail.com>
2019-01-22 17:35:27 -0500

    Added cassette-specific examples

---
+++ examples/cassettelive1.jl
@@ -0,0 +1,30 @@
+43
+
+2
+
+1
+
+x=6
+y=5
+
+y+32
+
+f1(x) = x+5
+
+3
+
+f1(5)
+
+function foo7(x)
+  y = 0
+       if (x > 0)
+            y+100
+        elseif (x == 0)
+            x+5;
+        elseif (x < 10)
+            -x;
+        else 0
+   end
+end
+foo7(10)
+      
